4018fa2c16f7504d95f828b1ecb5ead09d33a703
Christian Ramelow <info@codekandis.net>
2024-05-17 20:33:34 +0200

    Renames arguments.

--- src/ConfigurationInterface.php
+++ src/ConfigurationInterface.php
@@ -25,9 +25,9 @@ interface ConfigurationInterface
 
 	/**
 	 * Reads a value from the plain configuration or a default value.
-	 * @param mixed $default The default value to return if the value does not exist.
+	 * @param mixed $defaultValue The default value to return if the value does not exist.
 	 * @param string[] $indices The nested indices of the value to read.
 	 * @return mixed The read value if it exists, otherwise the default value.
 	 */
-	public function readOrDefault( mixed $default, string ...$indices ): mixed;
+	public function readOrDefault( mixed $defaultValue, string ...$indices ): mixed;
 }
